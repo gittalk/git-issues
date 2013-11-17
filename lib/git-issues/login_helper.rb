2e3256bbfa75b072781f525c4b5e838726b00cc9
Dominik Richter <dominik.richter@gmail.com>
2013-11-17 14:53:51 +0100

    add methods for setting oauth token and secret
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- lib/git-issues/login_helper.rb
+++ lib/git-issues/login_helper.rb
@@ -1,18 +1,31 @@
 require 'highline/import'
 
 module LoginHelper
-
-  def user; @user ||= get_user; end
-  def get_user
-    cli.ask("Enter username: "){|q| q.echo = true}
+  def oauth_token
+    @oauth_token ||= get_open("Enter OAuth token: ")
   end
 
-  def password; @password ||= get_password; end
-  def get_password
-    cli.ask("Enter password for user '#{user}': "){|q| q.echo = ''}
+  def oauth_secret
+    @oauth_secret ||= get_secret("Enter OAuth secret: ")
+  end
+
+  def user
+    @user ||= get_open("Enter username: ")
+  end
+
+  def password
+    @password ||= get_secret("Enter password for user '#{user}': ")
   end
 
   private
+
+  def get_open prompt
+    cli.ask(prompt){|q| q.echo = true}
+  end
+
+  def get_secret prompt
+    cli.ask(prompt){|q| q.echo = ''}
+  end
 
   def cli; @cli ||= HighLine.new; end
 
--- test/classes/login_helper_spec.rb
+++ test/classes/login_helper_spec.rb
@@ -5,6 +5,14 @@ describe LoginHelper do
     @l = Class.new do; include LoginHelper end.new
   end
 
+  it "should have a method to provide an oauth_token" do
+    @l.must_respond_to :oauth_token
+  end
+
+  it "should have a method to provide an oauth_secret" do
+    @l.must_respond_to :oauth_secret
+  end
+
   it "should have a method to provide a username" do
     @l.must_respond_to :user
   end
