ff6d003f60bd9cdddafd87db6ba04c54a67924cd
Marcos Piccinini <nofxx@users.noreply.github.com>
2015-08-08 03:42:41 -0300

    Accepts gitlab@ as user
    I believe archlinux AUR pkg has this as default user. 
    Now it recognizes it as gitlab repo.

--- lib/git-issues/providers/gitlab.rb
+++ lib/git-issues/providers/gitlab.rb
@@ -6,7 +6,7 @@ require 'gitlab'
 class RepoProvider::Gitlab
 
   URL_PATTERNS = [
-    /^(ssh:\/\/)?git@(?<host>[^:]*):(?<user>[^\/]+)\/(?<repo>.+)\.git$/
+    /^(ssh:\/\/)?git(lab)?@(?<host>[^:]*):(?<user>[^\/]+)\/(?<repo>.+)\.git$/
   ]
 
   def self.get_repo url
